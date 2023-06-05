fb6d70a8e510e9d9650c852ed3bd13a9d01283c0
Chernychova Marina <marina3123273@mail.ru>
2023-06-05 16:36:29 +0300

    Task solution

---
+++ src/test/java/ru/netology/javaqa/ShopRepositoryTest.java
@@ -0,0 +1,45 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+
+    @Test
+    public void testWhenIdExists (){
+        ShopRepository repository = new ShopRepository();
+        Product product1 = new Product(1, "Кружка", 5000);
+        Product product2 = new Product(2, "Термос", 5500);
+        Product product3 = new Product(3, "Котелок", 6000);
+
+        repository.add(product1);
+        repository.add(product2);
+        repository.add(product3);
+
+        repository.remove(2);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected,actual);
+
+
+    }
+    @Test
+    public void testWhenTrowsNotFoundException (){
+        ShopRepository repository = new ShopRepository();
+        Product product1 = new Product(1, "Кружка", 5000);
+        Product product2 = new Product(2, "Термос", 5500);
+        Product product3 = new Product(3, "Котелок", 6000);
+
+        repository.add(product1);
+        repository.add(product2);
+        repository.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class, ()->{
+            repository.remove(4);
+        });
+    }
+}
